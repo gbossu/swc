fdd228e2ced9d569135f5c6313c9c14b3433304f
Gaëtan Bossu <gaetan.bossu@telecom-sudparis.eu>
2018-12-21 09:40:04 +0100

    Add basic command-line parser

--- main.cpp
+++ main.cpp
@@ -2,22 +2,40 @@
 #include <QApplication>
 #include <QtGui>
 #include <QtWidgets>
-#include <iostream>
 
 int main(int argc, char **argv)
 {
+    QPointer<AnimatedContainer> container = nullptr;
+    bool foundOption = false;
+
     QApplication app(argc, argv);
+    QApplication::setApplicationName("SWC");
 
-//    if (app.arguments().count() != 2) {
-//        qFatal("Error: expected executable path as argument");
-//        return 1;
-//    }
+    QCommandLineParser parser;
+    parser.setApplicationDescription("Embed windows easily and animate them");
+    parser.addHelpOption();
 
-    AnimatedContainer w("swc");
-//    w.setWindowFlag(Qt::X11BypassWindowManagerHint);
-    w.setWindowFlag(Qt::WindowStaysOnTopHint);
-//    w.setWindowFlag(Qt::Desktop);
-//    w.setWindowFlag(Qt::BypassWindowManagerHint);
-    w.show();
+    QCommandLineOption classNameOption(QStringList() << "c" << "class-name",
+                                       "Class name of the window to embed",
+                                       "className");
+    parser.addOption(classNameOption);
+
+    parser.process(app);
+    if (!foundOption && parser.isSet(classNameOption)) {
+        container = new AnimatedContainer(parser.value(classNameOption));
+        foundOption = true;
+    }
+
+    // Exit if no option was found
+    if (!foundOption) {
+        parser.showHelp();
+    }
+
+//    container->setWindowFlag(Qt::X11BypassWindowManagerHint);
+    container->setWindowFlag(Qt::WindowStaysOnTopHint);
+//    container->setWindowFlag(Qt::Desktop);
+//    container->setWindowFlag(Qt::BypassWindowManagerHint);
+    container->show();
+
     return app.exec();
 }
